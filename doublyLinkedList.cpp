9f7576e7fe93f99de177023db66b0b3adbb3133b
SHIMANOEYE <shimanoee@yandex.ru>
2023-09-27 21:45:01 +0300

    finally

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -1,14 +1,14 @@
 // Двухсвязный список
 // Каждый узел содержит элемент и два указателя, на следующий и предыдущий.
 
-/* Операции : 1) Создание пустого
-              2) Проверка на пустоту
-              3) Очистка всего списка
-              4) Добавление после заданного
-              5) Добавление перед заданным
-              6) Удаление после заданного
-              7) Удаление перед заданным
-              8) Удаление текущего
+/* Операции : + 1) Создание пустого
+              + 2) Проверка на пустоту
+              + 3) Очистка всего списка
+              + 4) Добавление после заданного
+              + 5) Добавление перед заданным
+              + 6) Удаление после заданного
+              + 7) Удаление перед заданным
+              + 8) Удаление текущего
 */
 #include <iostream>
 
@@ -42,77 +42,135 @@ DList init(DList& head) {
 
 bool isEmpty(const DList& head) { return head == nullptr; }
 
-// void addToTail(dtype elem, DList& tail) {
-//   DList p = new Node_d(elem);
-//   tail->next = p;
-//   p->next = tail;
-//   tail = p;
-// }
-
-// void addAfterNode(DList pnode, dtype elem) {
-//   DList p = new Node_d(elem);
-//   p->next = pnode->next;
-//   pnode->next = p;
-//   p->prev = pnode;
-//   p->next->prev = p;
-// }
-
-void addAfterNode(DList pnode, DList& tail, dtype elem) {
-  DList p = new Node_d(elem);
-  p->next = pnode->next;
-  pnode->next = p;
-  p->prev = pnode;
-  if (pnode == tail) {
-    tail = p;
+void printDList(const DList& head, const DList& tail) {
+  DList temp = head;
+  if (!isEmpty(head)) {
+    while (temp != nullptr) {
+      cout << temp->data << " ";
+      temp = temp->next;
+    }
+    cout << endl;
   } else {
-    p->next->prev = p;
+    cout << "Empty list";
   }
 }
 
-void addBeforeNode(DList pnode, DList& head, dtype elem) {}  // написать
+void addAfterNode(const DList& curNode, DList& tail, const dtype& elem) {
+  DList newNode = new Node_d(elem);
 
-void deleteAfterNode(DList pnode, DList& tail) {
-  DList p = pnode->next;
-  pnode->next = p->next;
-  if (pnode->next != tail) {
-    p->next->prev = pnode;
+  newNode->next = curNode->next;
+  curNode->next = newNode;
+  newNode->prev = curNode;
+
+  if (curNode == tail) {
+    tail = newNode;
   } else {
-    tail = pnode;
+    newNode->next->prev = newNode;
   }
-  p->next = nullptr;
-  p->prev = nullptr;
-  delete p;
-  p = nullptr;
 }
 
-void deleteBeforeNode(DList pnode, DList& head) {}  // написать
+void addBeforeNode(const DList& curNode, DList& head, const dtype& elem) {
+  DList newNode = new Node_d(elem);
 
-void deleteCurrent(DList &pnode, DList &head, DList &tail) {
-  if (pnode != head) {
-    pnode->prev->next = pnode->next;
-  } else {
-    head = pnode->next;
-  }
+  newNode->next = curNode;
+  newNode->prev = curNode->prev;
+  curNode->prev = newNode;
 
-  if (pnode != tail) {
-    pnode->next->prev = pnode->prev;
+  if (curNode == head) {
+    head = newNode;
   } else {
-    tail = pnode->prev;
+    newNode->prev->next = newNode;
   }
-  pnode->next = nullptr;
-  pnode->prev = nullptr;
-  delete pnode;
+}
+
+void addToHead(DList& head, DList& tail, const dtype& elem) {
+  if (isEmpty(head)) {
+    head = new Node_d(elem);
+    tail = head;
+  } else {
+    addBeforeNode(head, head, elem);
+  }
+}
+
+void deleteAfterNode(const DList& curNode, DList& tail) {
+  DList delNode = curNode->next;
+  if (delNode != nullptr) {
+    curNode->next = delNode->next;
+    if (delNode == tail) {
+      tail = curNode;
+    } else {
+      delNode->next->prev = curNode;
+    }
+
+    delNode->next = nullptr;
+    delNode->prev = nullptr;
+    delete delNode;
+    delNode = nullptr;
+  }
+}
+
+void deleteBeforeNode(const DList& curNode, DList& head) {
+  DList delNode = curNode->prev;
+  if (delNode != nullptr) {
+    curNode->prev = delNode->prev;
+    if (delNode == head) {
+      head = curNode;
+    } else {
+      delNode->prev->next = curNode;
+    }
+
+    delNode->next = nullptr;
+    delNode->prev = nullptr;
+    delete delNode;
+    delNode = nullptr;
+  }
+}
+
+void deleteCurrentNode(DList* head_ref, DList delNode) {
+  if (*head_ref == nullptr || delNode == nullptr)
+    return;
+
+  if (*head_ref == delNode)
+    *head_ref = delNode->next;
+
+  if (delNode->next != nullptr)
+    delNode->next->prev = delNode->prev;
+
+  if (delNode->prev != nullptr)
+    delNode->prev->next = delNode->next;
+
+  delete delNode;
 }
 
 void Clear(DList& head, DList& tail) {
   while (!isEmpty(head)) {
-    deleteCurrent(head, head, tail);
+    deleteCurrentNode(&head, head);
   }
 }
 
 int main() {
   DList head, tail;
+  DList head_c, tail_c;
   init(head, tail);
-
-  cout << "123";
+  addToHead(head, tail, 4);
+  addToHead(head, tail, 3);
+  addToHead(head, tail, 2);
+  addToHead(head, tail, 1);
+  head_c = head;
+  tail_c = tail;
+  addAfterNode(tail, tail, 5);
+  printDList(head, tail);
+  deleteAfterNode(tail, tail);
+  printDList(head, tail);
+  deleteAfterNode(tail->prev, tail);
+  printDList(head, tail);
+  deleteBeforeNode(head, head);
+  printDList(head, tail);
+  deleteBeforeNode(head->next, head);
+  printDList(head, tail);
+  // cout << head->data << " " << tail->data << endl;
+  deleteCurrentNode(&head, head);
+  // cout << tail->data << endl;
+  //cout << head << " " << tail;
+   printDList(head, tail);
 }
